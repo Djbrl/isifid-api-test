ae266c8d1a3f3ba30dbedda588f04afa2b46b972
Djbrl <sydismyname123@gmail.com>
2020-09-29 05:21:23 +0200

    initial commit

---
+++ src/main/java/isifid/techtestdsy/TechtestDsyApplication.java
@@ -0,0 +1,12 @@
+package isifid.techtestdsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtestDsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtestDsyApplication.class, args);
+	}
+}
